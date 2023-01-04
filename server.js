0ff4f111105f184b00aaf9b139d26de2264fde7c
naga7 <naga@wishup.co>
2023-01-04 19:47:17 +0530

    #v2

--- server.js
+++ server.js
@@ -2,11 +2,12 @@ require("dotenv").config();
 const dbConnect = require("./app/DB/dbConnect");
 const paths = require("./app/routes/paths");
 const express = require("express");
+const cors = require("cors");
 
 const app = express();
 
 app.use(express.json());
-
+app.use(cors())
 app.use(
   express.urlencoded({
     extended: false,
