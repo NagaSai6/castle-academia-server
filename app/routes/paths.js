91f60d56da668fd7e338e2fd0543400a6747ca89
naga7 <naga@wishup.co>
2023-01-05 14:50:46 +0530

    #v7

--- app/routes/paths.js
+++ app/routes/paths.js
@@ -4,7 +4,8 @@ const publicUserController = require("../controllers/publicUserController");
 const paths = (app)=>{
    app.get("/",publicUserController().homePage)
    app.post("/sign-in",publicUserController().handleLogin);
-   app.post("/send-mail",publicUserController().handleUserFormSubmission)
+   app.post("/send-mail-form-submission",publicUserController().handleUserFormSubmission);
+   app.post("/send-mail-chatbot",publicUserController().handleUserFormSubmission)
 }
 
 module.exports = paths;
